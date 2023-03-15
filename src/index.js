8c768d34a08f0106daa515e62a5785f069e2ab32
Abdullah <abdullahkhrief@gmail.com>
2023-03-15 22:31:35 +0300

    Added the list to the page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+let url = "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/c18mtm405VaHSCO6kXTh/scores"
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = '';
+let submit = (name, score) => fetch(url, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json; charset=UTF-8",
+  },
+  body: JSON.stringify({
+    "user": name,
+    "score": score
+  })
+})
 
-  return element;
-}
+ .then(res => {
+    return res.json()})
+  .then(data => console.log(data))
 
-document.body.appendChild(component());
+
+// let submitBut = document.querySelector(".submit-but")
+
+// let nameVal = document.querySelector(".name").value
+// let scoreVal = document.querySelector(".score").value
+
+
+// submitBut.addEventListener("click", submit(nameVal, scoreVal))
+// submit("asdf", 232)
+
+
+    fetch(url)
+    .then(res => {
+      return res.json()
+    })
+    .then(data => {
+      let i = 0;
+      data.result.forEach(user => {
+
+        const newname = `<li>${data.result[i].user}:${data.result[i].score}</li>`
+        const list = document.querySelector("ul")
+        list.innerHTML += newname
+        i++
+      });
+    })
+    
