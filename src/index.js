4f01504e840715bd67238e91a3dbd548db392d75
Abdullah <abdullahkhrief@gmail.com>
2023-03-16 13:09:08 +0300

    Added the ability to add your score to the API

--- src/index.js
+++ src/index.js
@@ -2,26 +2,51 @@ import './style.css';
 
 let url = "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/c18mtm405VaHSCO6kXTh/scores"
 
-let submit = (name, score) => fetch(url, {
+// let submit = (user, score) => fetch(url, {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json; charset=UTF-8",
+//   },
+//   body: JSON.stringify({
+//     "user": user,
+//     "score": score
+//   })
+// })
+
+//  .then(res => {
+//     return res.json()})
+//   .then(data => console.log(data))
+
+const submit = async (user, score) => { 
+  let response = await fetch( url, {
   method: "POST",
   headers: {
-    "Content-Type": "application/json; charset=UTF-8",
+    "Content-Type": "application/json",
   },
   body: JSON.stringify({
-    "user": name,
-    "score": score
+    user,score
   })
 })
 
- .then(res => {
-    return res.json()})
-  .then(data => console.log(data))
+ 
+  let data = await response.json()
+  console.log(data)
+  return data
+}
+
+let submitForm = document.querySelector(".submit")
+
+submitForm.addEventListener("submit", (e) => {
+  let scoreVal = document.querySelector(".score").value
+  let nameVal = document.querySelector(".name").value
+
+  submit(nameVal, scoreVal)
+  console.log("worked")
+  e.preventDefault()
+})
 
 
-// let submitBut = document.querySelector(".submit-but")
 
-// let nameVal = document.querySelector(".name").value
-// let scoreVal = document.querySelector(".score").value
 
 
 // submitBut.addEventListener("click", submit(nameVal, scoreVal))
@@ -33,13 +58,20 @@ let submit = (name, score) => fetch(url, {
       return res.json()
     })
     .then(data => {
-      let i = 0;
+
+      console.log(data)
       data.result.forEach(user => {
 
-        const newname = `<li>${data.result[i].user}:${data.result[i].score}</li>`
+        const newname = `<li>${user.user}:${user.score}</li>`
         const list = document.querySelector("ul")
         list.innerHTML += newname
-        i++
+
       });
+     
     })
     
+  // const get = async () => {
+  //   const res = await fetch(url);
+
+  //   const data =
+  // }
